b9c3e6a002c52453fcdfda5f471b1ea6a9a6f52e
Devashish Priyadarshi <devashish234073@gmail.com>
2023-11-07 09:45:14 +0530

    Update automation.sh

--- gcp/create-kubernetes-cluster-rollout-image-update/automation.sh
+++ gcp/create-kubernetes-cluster-rollout-image-update/automation.sh
@@ -6,7 +6,7 @@ gcloud container clusters get-credentials $3 --region $2 --project $1
 gcloud artifacts repositories create $4 --location=$2 --repository-format=docker
 gcloud auth configure-docker $2-docker.pkg.dev
 docker build -t $5 .
-docker tag nodeapp $2-docker.pkg.dev/$1/$4/$5:$6
+docker tag $5 $2-docker.pkg.dev/$1/$4/$5:$6
 docker push $2-docker.pkg.dev/$1/$4/$5:$6
 kubectl create deployment $7 --image=$2-docker.pkg.dev/$1/$4/$5:$6
 kubectl expose deployment $7 --port=8080 --target-port=$8 --type=LoadBalancer
