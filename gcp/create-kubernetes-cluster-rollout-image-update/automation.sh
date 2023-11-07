35f01e62fae1edf775b7257c776485b8db200a72
Devashish Priyadarshi <devashish234073@gmail.com>
2023-11-07 08:57:42 +0530

    Update automation.sh

--- gcp/create-kubernetes-cluster-rollout-image-update/automation.sh
+++ gcp/create-kubernetes-cluster-rollout-image-update/automation.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 gcloud auth login
-gcloud container --project "yootooo" clusters create-auto "$3" --region "$2" --release-channel "regular" --network "projects/$1/global/networks/default" --subnetwork "projects/$1/regions/$2/subnetworks/default" --cluster-ipv4-cidr "/17"
+gcloud config set project $1
+gcloud container --project "$1" clusters create-auto "$3" --region "$2" --release-channel "regular" --network "projects/$1/global/networks/default" --subnetwork "projects/$1/regions/$2/subnetworks/default" --cluster-ipv4-cidr "/17"
+gcloud container clusters get-credentials $3 --region $2 --project $1
+gcloud artifacts repositories create $4 --location=$2 --repository-format=docker
 
